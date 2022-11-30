c85c0731cf52a784825824c482498c95734a3f0c
Cesar Vega <g.vega.cl@gmail.com>
2022-11-29 21:40:57 -0500

    Add fun facts

--- src/components/LoadingPage.tsx
+++ src/components/LoadingPage.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 
 const LoadingPage = () => {
-    const randomNumber = Math.random();
-    let funFact;
-    if(randomNumber < .3) {
-        funFact = "The Earl of Sandwich, John Montagu, who lived in the 1700s, reportedly invented the sandwich so he wouldn’t have to leave his gambling table to eat";
-    } else if (randomNumber < .6) {
-        funFact = "Elephants can't jump";
-    } else {
-        funFact = "There are no muscles in your fingers: Their function is controlled by muscles in your palms and arms.";
-    }
+    const funFacts = ["The Earl of Sandwich, John Montagu, who lived in the 1700s, reportedly invented the sandwich so he wouldn’t have to leave his gambling table to eat", "Elephants can't jump",
+"There are no muscles in your fingers: Their function is controlled by muscles in your palms and arms."];
 
     return (
         <div>
             <h2>We are loading your articles, please be patient, you will only need to wait once...</h2>
-            <h3 style={{marginTop: 20}}> <span style={{fontWeight:'bold'}}>Fun fact:</span> {funFact}</h3>
+            <h3 style={{marginTop: 20 }}>Fun facts:</h3>
+            <ul>
+                {funFacts.map((funFact) => {
+                    return(
+                        <li style={{marginTop: 10}}>
+                            <p>{funFact}</p>
+                        </li>
+                    )
+                })}
+
+            </ul>
         </div>
     );
 }
